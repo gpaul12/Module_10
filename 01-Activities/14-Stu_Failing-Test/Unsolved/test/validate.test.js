04736390cb6ea25bc0462bd86e6482c71a33b9d2
Gerry Paul <gpaul12@nc.rr.com>
2023-03-27 20:24:02 -0400

    13-14

--- 01-Activities/14-Stu_Failing-Test/Unsolved/test/validate.test.js
+++ 01-Activities/14-Stu_Failing-Test/Unsolved/test/validate.test.js
@@ -1,5 +1,15 @@
 // TODO: Import the Validate class.
+const Validate = require("../validate.js");
 
 // TODO: Describe a testing suite for checking the functionality of the Validate class.
+describe("Validate", () => {
+  // TODO: Write a test that asserts that isPassword() returns false when passed an empty string.
 
-// TODO: Write a test that asserts that isPassword() returns false when passed an empty string.
+  describe("invalid", () => {
+    it("should return false if passed an empty password", () => {
+      const str = "";
+      const result = new Validate().isPassword(str);
+      expect(result).toEqual(false);
+    });
+  });
+});
--- 01-Activities/14-Stu_Failing-Test/Unsolved/validate.js
+++ 01-Activities/14-Stu_Failing-Test/Unsolved/validate.js
@@ -1,3 +1,7 @@
 // TODO: Define a Validate class with an isPassword() function that takes in a password parameter.
+class Validate {
+  isPassword(pass) {}
+}
 
 // TODO: Export the Validate class from this file.
+module.exports = Validate;
