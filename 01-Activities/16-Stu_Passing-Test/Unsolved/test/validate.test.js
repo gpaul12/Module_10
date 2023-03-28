cd040b88b27c1e8da2c19701428b8ab81295664c
Gerry Paul <gpaul12@nc.rr.com>
2023-03-27 21:11:29 -0400

    16

--- 01-Activities/16-Stu_Passing-Test/Unsolved/test/validate.test.js
+++ 01-Activities/16-Stu_Passing-Test/Unsolved/test/validate.test.js
@@ -1,7 +1,19 @@
-const Validate = require('../validate.js');
+const Validate = require("../validate.js");
 
-describe('Validate', () => {
+describe("Validate", () => {
   // TODO: Write a test that checks if isPassword() returns false when the password is less than 8 characters.
+  describe("Password", () => {
+    it("should return false if password length is less than 8", () => {
+      const validate = new Validate();
+      const result = validate.isPassword("abc");
+      expect(result).toEqual(false);
+    });
+    it("should return false if password >7 characters", () => {
+      const validate = new Validate();
+      const result = validate.isPassword("wdawdawda");
+      expect(result).toEqual(true);
+    });
+  });
   // TODO: Write a test that checks if isPassword() returns false when the password does not contain at least 1 uppercase.
   // TODO: Write a test that checks if isPassword() returns false when the password does not contain at least 1 lowercase.
   // TODO: Write a test that checks if isPassword() returns false when the password does not contain at least 1 number.
--- 01-Activities/16-Stu_Passing-Test/Unsolved/validate.js
+++ 01-Activities/16-Stu_Passing-Test/Unsolved/validate.js
@@ -2,6 +2,12 @@ class Validate {}
 
 Validate.prototype.isPassword = (password) => {
   // TODO: Write code that checks if the password is less than 8 characters and returns false if so.
+  if (password.length < 8) {
+    return false;
+  } else {
+    return true;
+  }
+
   // TODO: Write code the checks if the password contains at least 1 uppercase, lowercase, and number. It should return true if it does, and false if not.
 };
 
